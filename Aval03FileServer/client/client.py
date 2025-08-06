855cb8f0d705ccf56715649e40a43fce319e0a22
Wellton <welltongabrielkkj19@gmail.com>
2025-08-06 20:42:55 -0300

    Comando Download por mascara adicionado

--- Aval03FileServer/client/client.py
+++ Aval03FileServer/client/client.py
@@ -7,16 +7,22 @@ def conectaAoServidor():
     global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((SERVIDOR, PORTA))
-    
+
 def mostraOpcoes():
-    print ("\n1 - Lista arquivos no servidor")
-    print ("2 - Donwload de um arquivo")
-    print ("3 - Fim")
-    return int (input("Escolha uma opção: "))
+    while True:
+        try:
+            print("\n1 - Lista arquivos no servidor")
+            print("2 - Download de um arquivo")
+            print("3 - Download de arquivos com máscara")
+            print("4 - Fim")
+            opcao = int(input("Escolha uma opção: "))
+            return opcao
+        except ValueError:
+            print("\nErro: entrada inválida, digite apenas números.")
 
 def adicionaTamanho(dados):
     tamanho = len(dados)
-    return tamanho.to_bytes(4, byteorder='big')+dados
+    return tamanho.to_bytes(4, byteorder='big') + dados
 
 def listaArquivos():
     comando = b"DIR"
@@ -25,30 +31,66 @@ def listaArquivos():
     while tamanhoListagem > 0:
         resposta = sock.recv(tamanhoListagem)
         tamanhoListagem -= len(resposta)
-        print (resposta.decode())
-    return
+        print(resposta.decode())
+
+def obterArquivo(nomeArquivo):
+    os.makedirs("downloads", exist_ok=True)
+    caminhoArquivo = os.path.join("downloads", nomeArquivo)
+
+    if os.path.exists(caminhoArquivo):
+        resposta = input(f"O arquivo '{nomeArquivo}' já existe. Sobrescrever? (s/n): ").lower()
+        if resposta != 's':
+            sock.send(adicionaTamanho(b"SKIP"))
+            print(f"Pulando o arquivo '{nomeArquivo}'.")
+            return
+        else:
+            sock.send(adicionaTamanho(b"OK"))
+    else:
+        sock.send(adicionaTamanho(b"OK"))
+
+    tamanhoArquivo = int.from_bytes(sock.recv(4), byteorder='big')
+    if tamanhoArquivo == 0:
+        print(f"Arquivo '{nomeArquivo}' não encontrado no servidor.")
+        return
+
+    dadosRecebidos = b""
+    while tamanhoArquivo > 0:
+        data = sock.recv(4096)
+        dadosRecebidos += data
+        tamanhoArquivo -= len(data)
+
+    with open(caminhoArquivo, "wb") as arquivo:
+        arquivo.write(dadosRecebidos)
+
+    print(f"Arquivo '{nomeArquivo}' salvo na pasta 'downloads'.")
 
 def downloadArquivo():
     comando = b"DOW"
     nomeArquivo = input('Digite o nome do arquivo que deseja baixar: ')
-    sock.send(adicionaTamanho(comando + b'&' + nomeArquivo.encode()))
+    sock.send(adicionaTamanho(comando + nomeArquivo.encode()))
 
-    tamanhoTotal = int.from_bytes(sock.recv(4), byteorder='big')
-    if tamanhoTotal == 0:
-        print("Arquivo não encontrado no servidor.")
+    obterArquivo(nomeArquivo)
+
+def downloadMascara():
+    comando = b"DMA"
+    mascara = input("Digite a máscara de arquivos (ex: *.jpg): ")
+    sock.send(adicionaTamanho(comando + mascara.encode()))
+
+    tamanhoLista = int.from_bytes(sock.recv(4), byteorder='big')
+    if tamanhoLista == 0:
+        print("Nenhum arquivo encontrado.")
         return
-
+    
     dadosRecebidos = b""
-    while tamanhoTotal > 0:
-        parte = sock.recv(min(4096, tamanhoTotal))
-        dadosRecebidos += parte
-        tamanhoTotal -= len(parte)
+    while tamanhoLista > 0:
+        data = sock.recv(4096)
+        dadosRecebidos += data
+        tamanhoLista -= len(data)
 
-    caminhoArquivo = os.path.join("downloads", nomeArquivo)
-    with open(caminhoArquivo, "wb") as arquivo:
-        arquivo.write(dadosRecebidos)
+    listaArquivos = dadosRecebidos.decode().split('\n')
 
-    print(f"\nArquivo '{nomeArquivo}' salvo na pasta 'downloads'.")
+    for nomeArquivo in listaArquivos:
+        obterArquivo(nomeArquivo)
 
 def main():
     conectaAoServidor()
@@ -59,9 +101,11 @@ def main():
         elif opcao == 2:
             downloadArquivo()
         elif opcao == 3:
+            downloadMascara()
+        elif opcao == 4:
             sock.close()
             return
         else:
-            print ("Opcao invalida!")
-            
+            print("Opção inválida!")
+
 main()
--- Aval03FileServer/server/server.py
+++ Aval03FileServer/server/server.py
@@ -1,7 +1,7 @@
-import socket, os, threading
+import socket, os, threading, glob
 
 SERVIDOR = ""
-PORTA    = 2121
+PORTA = 2121
 PASTAARQ = "arquivos"
 allClients = []
 
@@ -9,8 +9,8 @@ def escutaPorta():
     global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((SERVIDOR, PORTA))
-    sock.listen(1)
-    
+    sock.listen(5)
+
 def leComando():
     tamanhoComando = int.from_bytes(sockCon.recv(4), byteorder='big')
     comando = b''
@@ -30,36 +30,60 @@ def respondeComandoNulo():
 def respondeComandoDir():
     listaArquivos = os.listdir(PASTAARQ)
     resposta = ""
-
     for nomeArquivo in listaArquivos:
         caminhoCompleto = os.path.join(PASTAARQ, nomeArquivo)
         if os.path.isfile(caminhoCompleto):
             tamanhoArquivo = os.path.getsize(caminhoCompleto)
             resposta += f"{nomeArquivo} - {tamanhoArquivo} bytes\r\n"
-
     sockCon.send(adicionaTamanho(resposta.encode()))
 
-def respondeComandoDownload(nomeArquivo):
-    nomeArquivo = PASTAARQ + "/" + nomeArquivo
+def respondeComandoDow(nomeArquivo):
+    nomeArquivo = os.path.join(PASTAARQ, nomeArquivo)
+    if not os.path.exists(nomeArquivo):
+        sockCon.send((0).to_bytes(4, 'big'))
+        return
+
     tamArquivo = os.path.getsize(nomeArquivo)
     sockCon.send(tamArquivo.to_bytes(4, 'big'))
 
-    fd = open(nomeArquivo, "rb")
-    dados = fd.read(8192)
-    while dados != b'':
-        sockCon.send(dados)
+    with open(nomeArquivo, "rb") as fd:
         dados = fd.read(8192)
-    fd.close()
+        while dados:
+            sockCon.send(dados)
+            dados = fd.read(8192)
+
+def respondeComandoDMA(mascara):
+    caminhoMascara = os.path.join(PASTAARQ, mascara)
+    arquivosEncontrados = glob.glob(caminhoMascara)
+    nomesArquivos = [os.path.basename(arquivo) for arquivo in arquivosEncontrados]
+
+    if not nomesArquivos:
+        sockCon.send(adicionaTamanho(b''))
+        return
+
+    listaNomeArquivos = "\n".join(nomesArquivos).encode()
+    sockCon.send(adicionaTamanho(listaNomeArquivos))
+
+    for nomeArquivo in nomesArquivos:
+        respostaCliente = leComando()
+        if respostaCliente == b"SKIP":
+            continue
+        elif respostaCliente == b"OK":
+            respondeComandoDow(nomeArquivo)
 
 def processaComando(comando):
     if comando[:3] == b'DIR':
         respondeComandoDir()
     elif comando[:3] == b'DOW':
-        respondeComandoDownload(comando[4:].decode())
+        respondeComandoDow(comando[3:].decode()) 
+    elif comando[:3] == b'DMA':
+        respondeComandoDMA(comando[3:].decode())
     else:
         respondeComandoNulo()
 
-def trataCliente(sockCon, cliente):
+def trataCliente(sockConexao, cliente):
+    global sockCon
+    sockCon = sockConexao
     print(f"Tratando conexão com {cliente}")
     allClients.append(sockCon)
     try:
@@ -78,13 +102,11 @@ def trataCliente(sockCon, cliente):
         print("Fechando conexão porque o servidor caiu abruptamente.")
 
 def main():
-    global sockCon, cliente
-
     escutaPorta()
     print("Escutando conexões ....")
     while True:
-        sockCon, cliente = sock.accept()
+        sockConexao, cliente = sock.accept()
         print("Conexão recebida de", cliente)
-        threading.Thread(target=trataCliente, args=(sockCon, cliente)).start()
+        threading.Thread(target=trataCliente, args=(sockConexao, cliente)).start()
 
 main()
